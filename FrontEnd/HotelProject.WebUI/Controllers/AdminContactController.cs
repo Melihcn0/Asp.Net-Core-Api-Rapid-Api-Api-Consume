f1e6d35b2e5f5c083affe1f641e63a501cb4d188
Melihcan Kaya <Melihforbusiness@gmail.com>
2024-10-14 01:16:12 +0300

    No Commit Message

--- FrontEnd/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ FrontEnd/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -33,6 +33,18 @@ namespace HotelProject.WebUI.Controllers
             }
         }
 
+        public async Task<IActionResult> Sendbox()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:40699/api/SendMessage");
+            if (responseMessage.IsSuccessStatusCode) ;
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSendBoxDto>>(jsondata);
+                return View(values);
+            }
+        }
+
 
         [HttpGet]
         public IActionResult AddSendMessage()
@@ -63,5 +75,30 @@ namespace HotelProject.WebUI.Controllers
         {
             return PartialView();
         }
+        public async Task<IActionResult> MessageDetailsBySendbox(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:40699/api/SendMessage/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<GetMessageByIDDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        public async Task<IActionResult> MessageDetailsByInbox(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:40699/api/Contact/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<InboxContactDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
     }
 }
+
---
+++ FrontEnd/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,36 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AdminMailViewModel model)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxAddressfrom = new MailboxAddress("HotelierAdmin", "kayamelihcan67@gmail.com");
+            mimeMessage.From.Add(mailboxAddressfrom);
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+            var bodybuilder = new BodyBuilder();
+            bodybuilder.TextBody = model.Body;
+            mimeMessage.Body = bodybuilder.ToMessageBody();
+            mimeMessage.Subject = model.Subject;
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("kayamelihcan67@gmail.com", "rpmx yimm dsmg spxl");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+            return View();
+        }
+    }
+}
